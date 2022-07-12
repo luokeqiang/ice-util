8e74d441ffcde3cb8e3920a6641ef620fb9f3e65
luokeqiang <luoke9681@163.com>
2022-07-12 10:29:01 +0800

    新增ice-util-commom 工程

---
+++ ice-util-common/src/main/java/org/luoke/ice/util/common/annotation/Nullable.java
@@ -0,0 +1,5 @@
+package org.luoke.ice.util.common.annotation;/**
+* @author luoke
+* @date 2022/7/12 10:24
+*/public class Nullable {
+}
---
+++ ice-util-core/src/main/java/org/luoke/ice/util/core/collection/CollectionUtil.java
@@ -0,0 +1,5 @@
+package org.luoke.ice.util.core.collection;/**
+* @author luoke
+* @date 2022/7/12 10:11
+*/public class CollectionUtil {
+}
